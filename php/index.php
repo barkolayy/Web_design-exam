f95025a54ab33a9d45ac5f34417f05d308050807
Barkalov Nikolay <barcalovn@yandex.ru>
2022-06-19 04:09:30 +0700

    глава 74

--- php/index.php
+++ php/index.php
@@ -2,10 +2,11 @@
 	require_once 'form.php';
 	require_once 'input.php';
 	require_once 'submit.php';	
-	
+	require_once 'password.php';
 	$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'GET']);
 	echo $form->open();
-		echo (new Input)->setAttr('name', 'year');
+		echo (new Input)->setAttr('name', 'login');
+		echo (new Password)->setAttr('name', 'passw');
 		echo new Submit;
 	echo $form->close();
 ?>
