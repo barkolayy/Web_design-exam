405727c3bcdac0deefb56b1a08c8858abc5c0ad0
Barkalov Nikolay <barcalovn@yandex.ru>
2022-06-19 04:05:03 +0700

    73 глава

--- php/index.php
+++ php/index.php
@@ -1,30 +1,11 @@
 <?php
 	require_once 'form.php';
-	require_once 'input.php';	
-	$result = 0;
-	$form = (new Form)->setAttrs(['action' => '', 'method' => 'GET']);
-
-	$inp_1 = new Input();
-	$inp_2 = new Input();
-	$inp_3 = new Input();
-	$inp_4 = new Input();
-	$inp_5 = new Input();
-	$inp_sub = new Input();
-
-	echo $form->open();
-		echo $inp_1->setAttr('name', 'first');
-		echo $inp_2->setAttr('name', 'second');
-		echo $inp_3->setAttr('name', 'third');	
-		echo $inp_4->setAttr('name', 'fourth');
-		echo $inp_5->setAttr('name', 'fiveth');
-		echo $inp_sub->setAttr('type', 'submit');
-	echo $form->close();
-
+	require_once 'input.php';
+	require_once 'submit.php';	
 	
-
-	for ($i=1; $i<=5 ; $i++) { 
-		$curClass = 'inp_'. $i;
-		$result +=  (float) $$curClass->getAttr('value');			
-	}
-	echo $result;
+	$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'GET']);
+	echo $form->open();
+		echo (new Input)->setAttr('name', 'year');
+		echo new Submit;
+	echo $form->close();
 ?>
---
+++ php/submit.php
@@ -0,0 +1,8 @@
+<?php
+	class Submit extends Input{	
+		public function __construct(){		
+			$this->setAttr('type', 'submit');
+			parent::__construct();
+		}
+	}
+?>
