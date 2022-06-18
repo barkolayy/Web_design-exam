159707aef7368e67c001953c971b03319fe19e0b
Barkalov Nikolay <barcalovn@yandex.ru>
2022-06-19 00:03:19 +0700

    71 глава

--- php/index.php
+++ php/index.php
@@ -1,20 +1,10 @@
 <?php
-	require_once 'htmllist.php';
-	require_once 'tag.php';
-	require_once 'ullist.php';
-	require_once 'ollist.php';
-	$list1 = new UL;
-	$list2 = new Ol;
+	require_once 'form.php';
+	$form = (new Form)->setAttrs([
+		'action' => 'test.php',
+		'method' => 'POST'
+	]);
 	
-	echo $list1
-		->addItem((new ListItem())->setText('item1'))
-		->addItem((new ListItem())->setText('item2'))
-		->addItem((new ListItem())->setText('item3'))
-		;
-
-	echo $list2
-		->addItem((new ListItem())->setText('item1'))
-		->addItem((new ListItem())->setText('item2'))
-		->addItem((new ListItem())->setText('item3'))
-		;
+	echo $form->open();
+	echo $form->close();
 ?>
