137b040783e3937a8fb930363036c6d2a6de5c34
Barkalov Nikolay <barcalovn@yandex.ru>
2022-06-18 21:17:31 +0700

    глава 68 задания 1-4

--- php/index.php
+++ php/index.php
@@ -1,5 +1,7 @@
 <?php
 	require_once 'tag.php';
+	require_once 'image.php';
+	echo (new Image())->setAttrs(['src' => 'https://javasea.ru/uploads/posts/2021-06/1623421358_mona-liza.jpg', 'width' => '300px', 'height' => '200px'])->open(); 
 
 ?>
 
