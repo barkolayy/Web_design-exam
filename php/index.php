fb6ab0387f436e53efffdf08a1b20faba8160377
Barkalov Nikolay <barcalovn@yandex.ru>
2022-06-18 19:15:23 +0700

    59 глава задания 1-3

--- php/index.php
+++ php/index.php
@@ -1,16 +1,7 @@
 <?php
-	require_once 'file.php';
-	$File = new File('text.txt');
-	echo $File->getPath(), '<br>';
-	echo $File->getDir(), '<br>';
-	echo $File->getName(), '<br>';
-	echo $File->getExt(), '<br>';
-	echo $File->getSize(), '<br>';
-	echo $File->getText(). '<br>';
-	echo $File->setText('other text'). '<br>';
-	echo $File->appendText(' + text'). '<br>';
-	echo $File->copy('dir/new.txt'). '<br>';
-	echo $File->delete(). '<br>';
-	echo $File->rename('rename.txt'). '<br>';
-	echo $File->replace('dir'). '<br>';
+	require_once 'tag.php';
+	$header = new Tag('header');
+	echo $header->open(). 'header сайта'. $header->close();
+	$img = new Tag('img src="https://javasea.ru/uploads/posts/2021-06/1623421358_mona-liza.jpg" width="200px" ');
+	echo $img->open();
 ?>
