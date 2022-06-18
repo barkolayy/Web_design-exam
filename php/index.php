944d65dc44334e7366a368c8afc5a0e824f3cfd7
Barkalov Nikolay <barcalovn@yandex.ru>
2022-06-18 20:11:12 +0700

    64 глава

--- php/index.php
+++ php/index.php
@@ -2,14 +2,13 @@
 	require_once 'tag.php';
 	/*$img = new Tag('img');
 	echo $img->setAttrs(['src' => 'https://javasea.ru/uploads/posts/2021-06/1623421358_mona-liza.jpg', 'width' => '200px'])->setAttr('id', 'mona-lisa')->removeAttr('id')->open();*/
-	$input = new Tag('input');
-
-	echo $input
-	->setAttr('id', 'test')
-	->setAttr('disabled', true)
-	->open();
 		
-		
+	echo (new Tag('input'))
+	->setAttr('name', 'name1')
+	->open();	
 
+	echo (new Tag('input'))
+	->setAttr('name', 'name2')
+	->open();	
 ?>
 
